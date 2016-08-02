00929fc4fa815c7dcae9817419aeb30cd0d32536
Jennyf19 <ferriesj@gmail.com>
2016-08-01 20:45:39 -0700

    Add project files.

---
+++ StructsAndEnums/Date.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace StructsAndEnums
+{
+    //method advances the date in the structure by one month
+    //after Dec., code will resets the month to January and
+    //advances the value of the year field by 1
+    struct Date
+    {
+        private int year;
+        private Month month;
+        private int day;
+
+        //public constructor that takes three parameters (int for year, enum month, int for day)
+        public Date(int ccyy, Month mm, int dd)
+        {
+            this.year = ccyy - 1900;
+            this.month = mm;
+            this.day = dd - 1;
+        }
+
+        public override string ToString()
+        {
+            string data = $"{this.month} {this.day + 1} {this.year + 1900}";
+            return data;
+        }
+
+       public void AdvanceMonth()
+        {
+            this.month++;
+            if (this.month == Month.December + 1)
+            {
+                this.month = Month.January;
+                this.year++;
+            }
+        }
+    }
+}
---
+++ StructsAndEnums/Program.cs
@@ -0,0 +1,52 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace StructsAndEnums
+{
+    class Program
+    {
+        private static Date weddingAnniversary;
+
+        static void doWork()
+        {
+            /*Month first = Month.January;
+            Console.WriteLine(first);
+            //increment the first variable and display its value to the console
+            first++;
+            Console.WriteLine(first);*/
+            Date defaultDate = new Date();
+            Console.WriteLine(defaultDate);
+
+            Date weddingAnniversary = new Date(2015, Month.July, 4);
+            Console.WriteLine(weddingAnniversary);
+
+            Date weddingAnniversityCopy = weddingAnniversary;
+            Console.WriteLine($"Value of copy is {weddingAnniversityCopy}");
+
+            weddingAnniversary.AdvanceMonth();
+            Console.WriteLine($"New value of weddingAnniversary is {weddingAnniversary}");
+            Console.WriteLine($"Value of copy is still {weddingAnniversityCopy}");
+
+            Console.ReadLine();
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
